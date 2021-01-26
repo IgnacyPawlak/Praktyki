e7b0a73343fb1ae9a7e8d8c1ed28da801b3e228c
Ignacy Pawlak <ignacy.pawlak@gmail.com>
2021-01-26 15:31:31 +0100

    change

--- Again/ViewModel/SettingsViewModel.cs
+++ Again/ViewModel/SettingsViewModel.cs
@@ -1,13 +1,49 @@
-﻿using GalaSoft.MvvmLight;
+﻿using Again.Model;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Configuration;
 
 namespace Again.ViewModel
 {
     public class SettingsViewModel:ViewModelBase
     {
+        public SettingsViewModel()
+        {
+            SavePath = Properties.Settings.Default.SavePath;
+            SelectedValue = Properties.Settings.Default.fromat;
+        }
+        private Browse browse = new Browse();
+
+        string _savePath;
+
+        public string SavePath { get { return _savePath; } set { this.Set(nameof(SavePath), ref _savePath, value); } }
+
+        string _selectedValue;
+
+        public string SelectedValue { get { return _selectedValue; } set { this.Set(nameof(SelectedValue), ref _selectedValue, value); } }
+
+        public string SelectedValueShort { get { return SelectedValue.Split(':').Last(); } }
+
+        public ICommand BrowseCommand => new RelayCommand(()=> ExecuteBrowseCommand());
+
+        private void ExecuteBrowseCommand()
+        {
+            SavePath = browse.BrowseMethod();
+        }
+
+        public ICommand SaveSettingsCommand => new RelayCommand(() => ExecuteSaveSettingsCommand());
+
+        private void ExecuteSaveSettingsCommand()
+        {
+            Properties.Settings.Default.SavePath = SavePath;
+            Properties.Settings.Default.fromat = SelectedValue;
+            Properties.Settings.Default.Save();
+        }
     }
 }
